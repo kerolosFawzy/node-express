afb20f281599da1674a0328b271ef7aa6fd48767
kerolosfawzy <kerofawzy2055@gmail.com>
2019-01-28 03:57:39 +0200

    first task

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express'),
     http = require('http'),
     morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    dishRouter = require('./routes/dishRouter');
+    bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
+const dishRouter = require('./routes/dishRouter');
 
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use('/dishes' , dishRouter;
+app.use('/dishes' , dishRouter);
+app.use('/dishes/:dishId' , dishRouter);
 
 app.use((req, res, next) => {
     console.log(req.headers);
